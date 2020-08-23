6478a1b7e0bc5e31c0d2db7121f64a780aec310c
Sai AR <krishnasai2012@hotmail.com>
2020-08-23 13:55:18 +0530

    created basic scrapping function, completed: * opens olx page * enters keyword * enters location, min_price, max_price if given and clicks search * load more entries till search results numbers matches necessary number of entries
    to do:
    * iterate through each entry and pull ad links
    * open links and scrape necessary details
    * store data and write them in small batches to a file in-case of network failures
    * log each step, to help to continue from where its left off during failures

--- olx_scrapper.py
+++ olx_scrapper.py
@@ -7,67 +7,104 @@ Date : 22/August/2020
 # Import Statements
 import pandas as pd
 from selenium.webdriver import Chrome
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import resources
+
 from time import sleep
 
 
 # function for scrapping data from olx site
-def get_data(*args, web_driver_path='/', no_of_entries=500, verbose= True):
+def get_links(keyword, location='', min_price='0', max_price='9223372036854776000',
+             web_driver_path='/', no_of_entries=100):
     """
     This function scraps data from olx site (https://www.olx.in) and stores it as CSV file and
     returns the number of entries it scrapped.
 
     usage :
-        get_data(*args, verbose= True, no_of_entries=500, web_driver_path='/')
+        get_links(keyword)
+        get_links(keyword, kwargs)
 
-    *args= [keyword, location, min_price, max_price]
-        keyword: item to search (String)
-        location: locality to confine the search (String)
-        min_price: minimum price value (String)
-        max_price: maximum price value (String)
+    keyword: item to search (String)
+
+    kwargs:
+    =======
+    location: locality to confine the search (String)
+
+    min_price: minimum price value (String)
+
+    max_price: maximum price value (String)
 
     web_driver_path = Chrome driver executable path
         path of chrome driver on your pc, by default within the project folder
 
-    no_of_entries= 500
-        number of search results to fetch, by default 500.
-
-    verbose= boolean (True (or) False)
-        prints verbose
+    no_of_entries= 100
+        number of search results to fetch, by default 100 (max 1000 due to site limit).
 
     """
 
-    keyword, location, min_price, max_price = args[0]
     # reference_url = "https://www.olx.in/"
     url = f"https://www.olx.in/"
     driver = Chrome(web_driver_path,)
     driver.get(url)
     driver.maximize_window()
+    sleep(2)
 
-    # inputs the location, keyword, min-max price vales and clicks search
+    # inputs the location, keyword and clicks search
     if location != '':
-        driver.find_element_by_xpath(
-            '//*[@id="container"]/header/div/div/div[2]/div/div/div[1]/div/div/input').clear()
-        driver.find_element_by_xpath(
-            '//*[@id="container"]/header/div/div/div[2]/div/div/div[1]/div/div/input').send_keys(location)
-
+        driver.find_element_by_xpath(resources.location_textbox).clear()
+        driver.find_element_by_xpath(resources.location_textbox).send_keys(location)
         sleep(2)
-        driver.find_element_by_xpath(
-            '//*[@id="container"]/header/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div[1]').click()
-    if min_price != '':
-        pass
-    if max_price != '':
-        pass
-    driver.find_element_by_xpath(
-        '//*[@id="container"]/header/div/div/div[2]/div/div/div[2]/div/form/fieldset/div/input').send_keys(keyword)
+        driver.find_element_by_xpath(resources.first_location_item).click()
 
-    driver.find_element_by_xpath('//*[@id="container"]/header/div/div/div[2]/div/div/div[3]').click()
+    driver.find_element_by_xpath(resources.keyword_textbox).send_keys(keyword)
+    driver.find_element_by_xpath(resources.search_button).click()
+
+    # After search results loads, appends url with min-max price vales and filters the results
+    driver.get(f'{driver.current_url}&filter=price_between_{min_price}_to_{max_price}')
+    # driver.find_element_by_xpath(resources.price_filter_button).click()
     sleep(5)
+
+    # Check whether no_of_entries is lesser or equal to actual number of results available on site
+    num_of_available_results = int(driver.find_element_by_class_name(resources.max_results_available).text.split(' ')[0])
+    print(num_of_available_results)
+    if num_of_available_results < no_of_entries:
+        print(f"The actual number of results available on site is less than number of entries requested."
+              f"Will try to load the maximum available {num_of_available_results} results, "
+              f"instead of specified {no_of_entries} qty")
+        no_of_entries = num_of_available_results
+
+    items = driver.find_elements_by_class_name(resources.result_items)
+    load_more_btn_dynamic_xpath = 22
+    while load_more_btn_dynamic_xpath-2 < no_of_entries:
+        resources.scroll_down(driver)
+        sleep(2)
+        print(load_more_btn_dynamic_xpath - 2)
+        load_button = \
+            f'//*[@id="container"]/main/div/section/div/div/div[4]/div[2]/div/div[2]/ul/li[{load_more_btn_dynamic_xpath}]/div/button'
+        load_button_present = EC.presence_of_element_located((By.XPATH, load_button))
+        WebDriverWait(driver, 10).until(load_button_present)
+        driver.find_element_by_xpath(load_button).click()
+        load_more_btn_dynamic_xpath += 20
+
+    items = driver.find_elements_by_class_name(resources.result_items)
+    print(len(items))
+    with open("links.txt", "a+") as file:
+        for i in items:
+            file.write(i.find_elements_by_tag_name('a')[0].get_property('href'))
     driver.quit()
     return 0
+
+
+def get_data(url):
+    driver = Chrome(web_driver_path, )
+    driver.get(url)
+    driver.maximize_window()
+    sleep(2)
 
 
 # main program starts here
 if __name__ == "__main__":
     driver_path = r"C:\Users\krish\Documents\common_project_resources\chromedriver.exe"
-    options = ['cars', 'chennai', '', '']
-    get_data(options, web_driver_path=driver_path)
+    get_links('car', 'chennai', min_price='50000', no_of_entries=1000, web_driver_path=driver_path)
---
+++ resources.py
@@ -0,0 +1,42 @@
+# Import statements
+from time import sleep
+
+# Webscrapper page elements
+
+# xpaths
+location_textbox = '//*[@id="container"]/header/div/div/div[2]/div/div/div[1]/div/div/input'
+keyword_textbox = '//*[@id="container"]/header/div/div/div[2]/div/div/div[2]/div/form/fieldset/div/input'
+first_location_item = '//*[@id="container"]/header/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div[1]'
+search_button = '//*[@id="container"]/header/div/div/div[2]/div/div/div[3]'
+min_price_textbox = '//*[@id="container"]/main/div/section/div/div/div[4]/div[1]/div/div[4]/div/div[2]/div/input[1]'
+max_price_textbox = '//*[@id="container"]/main/div/section/div/div/div[4]/div[1]/div/div[4]/div/div[2]/div/input[2]'
+price_filter_button = '//*[@id="container"]/main/div/section/div/div/div[4]/div[1]/div/div[4]/div/div[2]/div/a'
+
+# class names:
+max_results_available = '_3RsTo'
+result_items = 'EIR5N'
+load_more_button = 'rui-3sH3b rui-3K5JC rui-1zK8h'
+
+# Tag name
+# load_more_button = 'button'
+
+# Scrolling page
+# Source Stackoverflow
+# https://stackoverflow.com/questions/48850974/selenium-scroll-to-end-of-page-in-dynamically-loading-webpage
+# Answered by Ratmir Asanov "https://stackoverflow.com/users/7901720/ratmir-asanov"
+
+
+def scroll_down(driver):
+    """A method for scrolling the page."""
+    # Get scroll height.
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to the bottom.
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load the page.
+        sleep(2)
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
